9c1a51bc7d2a3efb141b5db74a405f757c963e96
AnaPau1603 <paula_rosales@comunidad.unam.mx>
2020-05-29 13:38:09 -0500

    FIX: Correcciónen Inicio_sesion_DB.php

--- dynamics/Inicio_sesion_DB.php
+++ dynamics/Inicio_sesion_DB.php
@@ -5,7 +5,7 @@ if (isset($_POST['Usuario']) && isset($_POST['psw'])) {
   include_once "bd.php";
   include_once "Encrypt_PassW.php";
   include_once "Filtrar.php";
-  if (preg_match('/^([A-Z]{4}\d{2}(0[1-9]|1[0-2])([0-3]1|[0-2][1-9])[A-Z\d]{3}|3(1[6-9]|2[0-2])\d{6}|\d{6})$/', $_POST['$Usuario']) &&
+  if (preg_match('/^([A-Z]{4}\d{2}(0[1-9]|1[0-2])([0-3]1|[0-2][1-9])[A-Z\d]{3}|3(1[6-9]|2[0-2])\d{6}|\d{6})$/', $_POST['Usuario']) &&
       preg_match('/^(?=[\w!#$@%&*^+-]*\d)(?=[\w!#$@%&*^+-]*[A-Z])(?=[\w!#@$%&*^+-]*[a-z])(?=[\w!#$%&*^+@-]*[!#$%&*@^+-])\S{8,100}$/', $_POST['psw'])) {
   $Usuario=Filtrar($_POST['Usuario']);
   $Contraseña=Filtrar($_POST['psw']);
